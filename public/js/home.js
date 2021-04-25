0d64fcb10d239d0f0b53b075034775e981d6b1e2
Jordan Kelly <profjjk@gmail.com>
2021-04-24 18:02:33 -0700

    add hover effects to projects

--- public/js/home.js
+++ public/js/home.js
@@ -1,27 +1,12 @@
-// Change active scroll point on click.
-$('.scroll-item').last().on('click', function() {
-  $('.scroll-item').first().removeClass('active');
-  $('nav').addClass('hide');
-  $(this).addClass('active');
-});
-$('.scroll-item').first().on('click', function() {
-  $('.scroll-item').last().removeClass('active');
-  $('nav').removeClass('hide');
-  $(this).addClass('active');
-});
-
-// Change active scroll point on scroll.
-const workSection = document.getElementById('work-section');
+// Change active scroll point on scrollbar.
 const sections = document.querySelectorAll('section');
 const topDot = document.querySelectorAll('.scroll-item')[0];
 const bottomDot = document.querySelectorAll('.scroll-item')[1];
-
 const options = {
   root: null,
   threshold: 0,
   rootMargin: '-40%'
 };
-
 const observer = new IntersectionObserver(function(entries, observer) {
   entries.forEach(entry => {
     if (entry.target.id === "greeting-section" && entry.isIntersecting === true) {
@@ -34,7 +19,6 @@ const observer = new IntersectionObserver(function(entries, observer) {
     }
   })
 }, options);
-
 sections.forEach(section => {
   observer.observe(section);
 })
